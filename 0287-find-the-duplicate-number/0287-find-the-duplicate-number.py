feab03504bcc68a69acaca066b1d92bafc7a5973
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-01-21 14:01:54 -0800

    Time: 26 ms (64.93%), Space: 30 MB (39.73%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,18 +4,19 @@ class Solution:
         nums = [1,3,4,2,2]
         indi = [0,1,2,3,4]
 
-        Linked List is 0 -> 1 (nums[0]) -> 3 (nums[1]) -> 2 (nums[3]) -> 4 (nums[2]) -> cycle to 2 again and so on...
+        Linked List is 1 (nums[0]) -> 3 (nums[1]) -> 2 (nums[3]) -> 4 (nums[2]) -> cycle to 2 again and so on...
+        (Don't think of trying with indices as it is confusing)
         For the above list, initially slow pointer moves 1 step and fast moves 2 steps starting from first element
         """
-        slow = 0
-        fast = 0
+        slow = nums[0]
+        fast = nums[0]
         slow = nums[slow]
         fast = nums[nums[fast]]
         while slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
         # According to floyd, if element e is starting point of a cycle, distance from current slow to this e is same as distance from fast starting from 1st element of LL and moving 1 step ahed to this e i.e., fast and slow intersect at e at certain point of time
-        fast = 0
+        fast = nums[0]
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
