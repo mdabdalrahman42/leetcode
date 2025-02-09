4fdff1a820502b149537e4d4cfdf7238b0dcb3f7
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-08 21:13:41 -0800

    Time: 0 ms (100%), Space: 19.6 MB (70.53%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,3 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return 2 * sum(set(nums)) - sum(nums)
+        # return 2 * sum(set(nums)) - sum(nums)
+        xor = 0
+        for i in nums:
+            xor ^= i
+        return xor
