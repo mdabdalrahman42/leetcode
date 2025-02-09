a45a156b080faa6cae4a2e57bf473d7b3563b8fa
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-08 17:30:48 -0800

    Time: 0 ms (100%), Space: 18.5 MB (51.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        """
         rows = [1 for i in range(len(matrix))]
         cols = [1 for i in range(len(matrix[0]))]
         for i in range(len(matrix)):
@@ -18,3 +19,28 @@ class Solution:
             if cols[j] == 0:
                 for i in range(len(matrix)):
                     matrix[i][j] = 0
+        """
+        row_1 = 1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i == 0:
+                    if matrix[i][j] == 0:
+                        row_1 = 0
+                else:
+                    if matrix[i][j] == 0:
+                        matrix[0][j] = 0
+                        matrix[i][0] = 0
+        for j in range(1, len(matrix[0])):
+            if matrix[0][j] == 0:
+                for i in range(1, len(matrix)):
+                    matrix[i][j] = 0
+        for i in range(1, len(matrix)):
+            if matrix[i][0] == 0:
+                for j in range(1, len(matrix[i])):
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for i in range(1, len(matrix)):
+                matrix[i][0] = 0
+        if row_1 == 0:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
