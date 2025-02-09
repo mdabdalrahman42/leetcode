0fc2ce9386fd444abb3390e4e79b9b897443ba7c
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-09 10:50:06 -0800

    Time: 28 ms (53.72%), Space: 30 MB (46.36%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,13 +1,20 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow = fast = nums[0] # or 0
+        """
+        for i in range(len(nums)):
+            if nums[abs(nums[i]) - 1] < 0:
+                return abs(nums[i])
+            else:
+                nums[abs(nums[i]) - 1] = - nums[abs(nums[i]) - 1]
+        """
+        slow = fast = 0
         slow = nums[slow]
         fast = nums[nums[fast]]
-        while slow != fast: # slow == fast means there is a cycle (i.e., some number repeated)
+        while slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
-        fast = nums[0] # or 0
-        while slow != fast: # According to floyd, distance from current slow and new fast (moving at speed 1) to number that led to cycle is equal and meet exactly at that number again
+        slow = 0
+        while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
         return slow
