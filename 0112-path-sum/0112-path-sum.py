69e3bbfb7be2bfacf71e2e1f97e1cac176617b73
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-10 12:44:14 -0800

    Time: 0 ms (100%), Space: 19.1 MB (21.96%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -7,15 +7,17 @@
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         def dfs(n, curr_sum):
-            if n:
-                curr_sum += n.val
-                if not n.left and not n.right:
-                    return curr_sum == targetSum
-                if n.left:
-                    if dfs(n.left, curr_sum):
-                        return True
-                if n.right:
-                    if dfs(n.right, curr_sum):
-                        return True
-            return False 
+            curr_sum += n.val
+            if not n.left and not n.right:
+                return curr_sum == targetSum
+            if n.left:
+                if dfs(n.left, curr_sum):
+                    return True
+            if n.right:
+                if dfs(n.right, curr_sum):
+                    return True
+            return False
+
+        if not root:
+            return False
         return dfs(root, 0)
