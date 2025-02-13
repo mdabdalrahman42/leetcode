523af52727006be86902f6497f4020c67cacc0ae
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-13 10:52:12 -0800

    Time: 0 ms (100%), Space: 18.8 MB (34.5%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -6,18 +6,18 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_dia = 0
-        queue = deque([[root, 1, 0]])
-        curr_level = 0
-        level_start = 1
+        max_width = 0
+        queue = deque([(root, 1, 0)])
+        level = 0
+        low_val = 1
         while queue:
-            node, heap_val, level = queue.popleft()
-            if level > curr_level:
-                curr_level = level
-                level_start = heap_val
+            node, curr_val, curr_level = queue.popleft()
+            if curr_level > level:
+                level = curr_level
+                low_val = curr_val
             if node.left:
-                queue.append([node.left, 2 * heap_val, level + 1])
+                queue.append((node.left, 2 * curr_val, curr_level + 1))
             if node.right:
-                queue.append([node.right, 2 * heap_val + 1, level + 1])
-            max_dia = max(max_dia, heap_val - level_start + 1)
-        return max_dia
+                queue.append((node.right, 2 * curr_val + 1, curr_level + 1))
+            max_width = max(max_width, curr_val - low_val + 1)
+        return max_width
