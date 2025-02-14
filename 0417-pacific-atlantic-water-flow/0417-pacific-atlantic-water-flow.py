d1c9753872daeeef5bb56627774698f198df0e90
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-13 16:56:22 -0800

    Time: 59 ms (19.72%), Space: 20.3 MB (5.71%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,30 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
         pacific = set()
         atlantic = set()
-        def bfs(r, c, visited):
-            visited.add((r, c))
-            queue = deque([(r, c, heights[r][c])])
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            while queue:
-                r, c, height = queue.popleft()
-                for new_row, new_col in directions:
-                    new_row += r
-                    new_col += c
-                    if new_row in range(rows) and new_col in range(cols) and (new_row, new_col) not in visited and heights[new_row][new_col] >= height:
-                        queue.append((new_row, new_col, heights[new_row][new_col]))
-                        visited.add((new_row, new_col))
-        for c in range(cols):
-            bfs(0, c, pacific)
-            bfs(rows - 1, c, atlantic)
-        for r in range(rows):
-            bfs(r, 0, pacific)
-            bfs(r, cols - 1, atlantic)
+        rows = len(heights)
+        cols = len(heights[0])
         output = []
+        def dfs(r, c, visited, height):
+            visited.add((r, c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for new_row, new_col in directions:
+                new_row += r
+                new_col += c
+                if (new_row, new_col) not in visited and new_row in range(rows) and new_col in range(cols) and heights[new_row][new_col] >= height:
+                    visited.add((new_row, new_col))
+                    dfs(new_row, new_col, visited, heights[new_row][new_col])
+
+        for c in range(cols):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(rows - 1, c, atlantic, heights[rows - 1][c])
+        for r in range(rows):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, cols - 1, atlantic, heights[r][cols - 1])
         for r in range(rows):
             for c in range(cols):
                 if (r, c) in pacific and (r, c) in atlantic:
                     output.append([r, c])
         return output
-        
