9927a871d5ba71922539ca89ea59d7db7f5d7e8c
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-15 11:51:22 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,20 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
+        rows = len(board)
+        cols = len(board[0])
 
         def bfs(r, c, i):
             queue = deque([(r, c, i, {(r, c)})])
-            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             while queue:
                 r, c, i, visited = queue.popleft()
-                
-                if i == len(word):  # Word found
+                if i == len(word):
                     return True
-
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc  # Compute new position
-                    
-                    if (nr, nc) not in visited and 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == word[i]:
-                        queue.append((nr, nc, i + 1, visited | {(nr, nc)}))
-            
+                for nr, nc in directions:
+                    new_row = r + nr
+                    new_col = c + nc
+                    if (new_row, new_col) not in visited and new_row in range(rows) and new_col in range(cols) and board[new_row][new_col] == word[i]:
+                        queue.append((new_row, new_col, i + 1, visited | {(new_row, new_col)}))
             return False
 
         for r in range(rows):
@@ -25,5 +22,4 @@ class Solution:
                 if board[r][c] == word[0]:
                     if bfs(r, c, 1):
                         return True
-
         return False
