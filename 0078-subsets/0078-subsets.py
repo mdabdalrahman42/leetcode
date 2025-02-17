e2bb545e99b9fc0114f6fd1dbf1d774f2c9a37af
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-16 21:49:26 -0800

    Time: 0 ms (100%), Space: 17.8 MB (65.44%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,5 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        """
         output = []
         subset = []
         def dfs(i):
@@ -12,10 +11,4 @@ class Solution:
             subset.pop()
             dfs(i + 1)
         dfs(0)
-        return output
-        """
-        output = [[]]
-        for i in nums:
-            for j in range(len(output)):
-                output.append(output[j] + [i])
         return output
