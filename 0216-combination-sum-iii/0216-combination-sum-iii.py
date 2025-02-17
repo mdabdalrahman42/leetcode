b92a34b4d6cef2370285b55acaaa20539a7d8376
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-16 17:36:05 -0800

    Time: 0 ms (100%), Space: 17.7 MB (86.53%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,15 +1,16 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res = []
-        def dfs(i, comb, curr_sum):
-            if len(comb) == k and curr_sum == n:
-                res.append(comb[:])
+        output = []
+        comb = []
+        def dfs(i, curr_sum):
+            if curr_sum == n and len(comb) == k:
+                output.append(comb.copy())
                 return
-            if len(comb) > k or curr_sum > n or i > 9:
+            if curr_sum > n or len(comb) > k or i > 9:
                 return
             comb.append(i)
-            dfs(i + 1, comb, curr_sum + i)
+            dfs(i + 1, curr_sum + i)
             comb.pop()
-            dfs(i + 1, comb, curr_sum)
-        dfs(1, [], 0)
-        return res
+            dfs(i + 1, curr_sum)
+        dfs(1, 0)
+        return output
