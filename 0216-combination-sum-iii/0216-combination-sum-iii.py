c7ab67f1f2cec2c4a6c911bbf82d61e231625ea1
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-16 23:27:47 -0800

    Time: 0 ms (100%), Space: 17.7 MB (86.53%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -4,9 +4,9 @@ class Solution:
         comb = []
         def dfs(i, curr_sum):
             if curr_sum == n and len(comb) == k:
-                output.append(comb.copy())
+                output.append(comb[:])
                 return
-            if curr_sum > n or len(comb) > k or i > 9:
+            if len(comb) > k or curr_sum > n or i > 9:
                 return
             comb.append(i)
             dfs(i + 1, curr_sum + i)
