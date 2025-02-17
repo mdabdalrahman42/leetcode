cc4d6565f1e5a66d37c1c58b8cc4654a80836b29
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-16 23:51:13 -0800

    Time: 47 ms (71.49%), Space: 34.6 MB (44.53%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -11,12 +11,13 @@ class Solution:
                 r -= 1
             return True
         def dfs(i):
-            if i >= len(s):
+            if i == len(s):
                 output.append(part[:])
                 return
             for j in range(i, len(s)):
+                sub = s[i:j + 1]
                 if ispali(i, j):
-                    part.append(s[i: j + 1])
+                    part.append(sub)
                     dfs(j + 1)
                     part.pop()
         dfs(0)
