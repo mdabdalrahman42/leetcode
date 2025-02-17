58acbb3c4b06dc1aeb186f5eee49f80158875dea
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-16 19:30:35 -0800

    Time: 155 ms (64.86%), Space: 18 MB (6.51%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-        def dfs(i, visited):
-            if i == len(s):
+        visited = set()
+        def dfs(i):
+            if i >= len(s):
                 return 0
             res = 0
             for j in range(i, len(s)):
-                if s[i:j + 1] not in visited:
-                    visited.add(s[i:j + 1])
-                    res = max(res, 1 + dfs(j + 1, visited))
-                    visited.remove(s[i: j + 1])
+                part = s[i:j + 1]
+                if part not in visited:
+                    visited.add(part)
+                    res = max(res, 1 + dfs(j + 1))
+                    visited.remove(part)
             return res
-        return dfs(0, set())
+        return dfs(0)
