2ef9dfa37a4611a1d52da7b16e92f541c8c61905
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-17 23:12:26 -0800

    Time: 0 ms (100%), Space: 18.1 MB (71.9%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,28 +1,23 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        top = 0
-        bot = rows - 1
-        while top <= bot:
-            mid = top + (bot - top) // 2
-            if target < matrix[mid][0]:
-                bot = mid - 1
-            elif target > matrix[mid][cols - 1]:
-                top = mid + 1
+        t, b = 0, len(matrix) - 1
+        while t <= b:
+            mid = t + (b - t) // 2
+            if target > matrix[mid][len(matrix[0]) - 1]:
+                t = mid + 1
+            elif target < matrix[mid][0]:
+                b = mid - 1
             else:
                 break
-        if not top <= bot:
+        if not t <= b:
             return False
-        row = top + (bot - top) // 2
-        l = 0
-        r = cols - 1
+        l, r = 0, len(matrix[0]) - 1
         while l <= r:
-            mid = l + (r - l) // 2
-            if matrix[row][mid] == target:
+            m = l + (r - l) // 2
+            if matrix[mid][m] == target:
                 return True
-            if matrix[row][mid] > target:
-                r = mid - 1
-            if matrix[row][mid] < target:
-                l = mid + 1
+            elif target > matrix[mid][m]:
+                l = m + 1
+            else:
+                r = m - 1
         return False
