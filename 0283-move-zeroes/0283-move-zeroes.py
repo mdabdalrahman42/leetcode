20134cb8c06f99f1c56d0fba2f7a04aaa16bca0f
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-18 11:48:13 -0800

    Time: 7 ms (47.12%), Space: 18.8 MB (78.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p1 = p2 = 0
+        p1 = 0
+        while p1 < len(nums) and nums[p1] != 0:
+            p1 += 1
+        p2 = p1 + 1
         while p2 < len(nums):
             if nums[p2] != 0:
                 nums[p1] = nums[p2]
