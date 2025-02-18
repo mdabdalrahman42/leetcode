d6666b1a7049925dee37b93a73d38b5f0b7294ae
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-18 00:29:05 -0800

    Time: 0 ms (100%), Space: 25.9 MB (6.58%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -3,11 +3,11 @@ class Solution:
         def bs(l, r):
             if l <= r:
                 mid = l + (r - l) // 2
-                if nums[mid] == target:
+                if target == nums[mid]:
                     return mid
-                if nums[mid] > target:
-                    return bs(l, mid - 1)
-                if nums[mid] < target:
+                if target > nums[mid]:
                     return bs(mid + 1, r)
+                else:
+                    return bs(l, mid - 1)
             return -1
         return bs(0, len(nums) - 1)
