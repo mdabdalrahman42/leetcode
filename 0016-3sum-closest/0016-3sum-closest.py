3ebe8afb7b7bfeb26aaedaffffcf8946f562862f
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-19 22:25:42 -0800

    Time: 364 ms (38.83%), Space: 17.7 MB (77.86%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,15 +4,16 @@ class Solution:
         closest = float("inf")
         for i in range(len(nums)):
             if not (i > 0 and nums[i] == nums[i - 1]):
-                l, r = i + 1, len(nums) - 1
+                l = i + 1
+                r = len(nums) - 1
                 while l < r:
-                    summ = nums[i] + nums[l] + nums[r]
-                    if abs(target - summ) < abs(target - closest):
-                        closest = summ
-                    if summ == target:
-                        return closest
-                    elif summ > target:
+                    curr_sum = nums[i] + nums[l] + nums[r]
+                    if abs(target - curr_sum) < abs(target - closest):
+                        closest = curr_sum
+                    if curr_sum > target:
                         r -= 1
-                    else:
+                    elif curr_sum < target:
                         l += 1
+                    else:
+                        return target
         return closest
