15e40ba4fe49a3e233170f36447c56b6239fe10e
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-23 13:45:03 -0800

    Time: 18 ms (69.76%), Space: 17.7 MB (87.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,30 +2,30 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        s1_char = [0 for i in range(26)]
-        s2_char = [0 for i in range(26)]
+        s1_chars = [0] * 26
+        s2_chars = [0] * 26
         for i in range(len(s1)):
-            s1_char[ord(s1[i]) - ord("a")] += 1
-            s2_char[ord(s2[i]) - ord("a")] += 1
+            s1_chars[ord(s1[i]) - ord("a")] += 1
+            s2_chars[ord(s2[i]) - ord("a")] += 1
         matches = 0
         for i in range(26):
-            if s1_char[i] == s2_char[i]:
+            if s1_chars[i] == s2_chars[i]:
                 matches += 1
         l = 0
         for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
             index = ord(s2[r]) - ord("a")
-            rem_index = ord(s2[l]) - ord("a")
-            s2_char[index] += 1
-            if s2_char[index] == s1_char[index]:
+            s2_chars[index] += 1
+            if s1_chars[index] == s2_chars[index]:
                 matches += 1
-            elif s2_char[index] == s1_char[index] + 1:
+            elif s2_chars[index] == s1_chars[index] + 1:
                 matches -= 1
-            s2_char[rem_index] -= 1
-            if s2_char[rem_index] == s1_char[rem_index]:
+            index = ord(s2[l]) - ord("a")
+            s2_chars[index] -= 1
+            if s1_chars[index] == s2_chars[index]:
                 matches += 1
-            elif s2_char[rem_index] == s1_char[rem_index] - 1:
+            elif s2_chars[index] == s1_chars[index] - 1:
                 matches -= 1
             l += 1
         return matches == 26
