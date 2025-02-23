d4dc66e8fab7ced5a843490c17955aedd2653b88
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-22 22:15:41 -0800

    Time: 203 ms (26.33%), Space: 23.6 MB (44.72%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        max_fruits = 0
+        total = 0
+        l = 0
+        count = {}
+        for r in range(len(fruits)):
+            if fruits[r] not in count:
+                count[fruits[r]] = 0
+            count[fruits[r]] += 1
+            total += 1
+            while len(count) > 2:
+                count[fruits[l]] -= 1
+                if not count[fruits[l]]:
+                    count.pop(fruits[l])
+                total -= 1
+                l += 1
+            max_fruits = max(max_fruits, total)
+        return max_fruits
