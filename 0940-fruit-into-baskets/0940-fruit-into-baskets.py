ecd4d384249a1606b5373eb43ef2d67a867a9b7a
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-23 18:49:32 -0800

    Time: 191 ms (44.86%), Space: 23.7 MB (44.7%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,8 +1,8 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        max_fruits = 0
         count = {}
         l = 0
-        max_fruits = 0
         for r in range(len(fruits)):
             if fruits[r] not in count:
                 count[fruits[r]] = 0
