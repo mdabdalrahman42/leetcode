e77b70e978c9be12ff063a64153de4da0e3cd3e8
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-23 17:26:53 -0800

    Time: 6 ms (57.26%), Space: 18.6 MB (67.48%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -5,14 +5,14 @@ class Solution:
         while p1 < len(firstList) and p2 < len(secondList):
             start1, end1 = firstList[p1]
             start2, end2 = secondList[p2]
-            if end2 < start1:
-                p2 += 1
-            elif end1 < start2:
+            if end1 < start2:
                 p1 += 1
+            elif end2 < start1:
+                p2 += 1
             else:
                 output.append([max(start1, start2), min(end1, end2)])
-                if end1 < end2:
-                    p1 += 1
-                else:
+                if end2 < end1:
                     p2 += 1
+                else:
+                    p1 += 1
         return output
