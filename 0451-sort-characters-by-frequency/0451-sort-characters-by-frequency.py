aa6909d6bd171c67b47d8f19ffd0af079004dab9
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-25 15:47:27 -0800

    Time: 40 ms (9.5%), Space: 27.7 MB (8.03%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -5,10 +5,11 @@ class Solution:
             if i not in count:
                 count[i] = 0
             count[i] += 1
-        heap = [[-count[i], i] for i in count]
-        heapq.heapify(heap)
+        bucket = [[] for i in s]
+        for i in count:
+            bucket[count[i] - 1].append(i)
         output = ""
-        for _ in range(len(count)):
-            cnt, char = heapq.heappop(heap)
-            output += char * (- cnt)
+        for i in range(len(bucket) - 1, -1, -1):
+            for j in bucket[i]:
+                output += j * (i + 1)
         return output
