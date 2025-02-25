01e5a41bb960c7d2b2e7342b0a773e551c454303
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-25 14:43:44 -0800

    Time: 139 ms (49.75%), Space: 19 MB (98.37%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -6,16 +6,16 @@ class Solution:
                 count[i] = 0
             count[i] += 1
         heap = [-cnt for cnt in count.values()]
-        time = 0
         heapq.heapify(heap)
         queue = deque([])
+        time = 0
         while heap or queue:
             time += 1
+            if heap:
+                cnt = heapq.heappop(heap) + 1
+                if cnt != 0:
+                    queue.append([cnt, time + n])
             if queue and queue[0][1] == time:
                 heapq.heappush(heap, queue[0][0])
                 queue.popleft()
-            if heap:
-                cnt = heapq.heappop(heap) + 1
-                if cnt:
-                    queue.append([cnt, time + n + 1])
         return time
