136143016abaa74fa7cc7209561b311396af3791
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-26 12:31:40 -0800

    Time: 0 ms (100%), Space: 17.7 MB (78.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = {n:1}
-        def dfs(curr_sum):
-            if curr_sum in dp:
-                return dp[curr_sum]
-            if curr_sum > n:
-                return 0
-            dp[curr_sum] = dfs(curr_sum + 1) + dfs(curr_sum + 2)
-            return dp[curr_sum]
-        dfs(0)
-        return dp[0]
+        one = 1
+        two = 1
+        for _ in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
