f9b8396bdbf9669f4db507ae1356e453eb95e27e
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-28 00:36:41 -0800

    Time: 578 ms (59.44%), Space: 20.1 MB (51.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,18 +3,11 @@ class Solution:
         if sum(nums) % 2 != 0:
             return False
         target = sum(nums) // 2
-        dp = {}
-        def dfs(curr_sum, i):
-            if curr_sum == 0:
-                return True
-            if curr_sum < 0 or i >= len(nums):
-                return False
-            if (curr_sum, i) in dp:
-                return dp[(curr_sum, i)]
-            if dfs(curr_sum - nums[i], i + 1):
-                return True
-            if dfs(curr_sum, i + 1):
-                return True
-            dp[(curr_sum, i)] = False
-            return False
-        return dfs(target, 0)
+        dp = set([0])
+        for i in nums[-1::-1]:
+            new_dp = set()
+            for n in dp:
+                new_dp.add(n + i)
+                new_dp.add(n)
+            dp = new_dp
+        return True if target in dp else False
