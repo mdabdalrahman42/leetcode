c8806d1f2b90d5d8ab3c82e62b11ed3e61d7de1a
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-02-27 18:52:50 -0800

    Time: 0 ms (100%), Space: 18 MB (31.22%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,6 +7,7 @@ class Solution:
             if i in dp:
                 return dp[i]
             if s[i] == "0":
+                dp[i] = 0
                 return 0
             res = dfs(i + 1)
             if i + 1 < len(s) and int(s[i] + s[i + 1]) in range(10, 27):
