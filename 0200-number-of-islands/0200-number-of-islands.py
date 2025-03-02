fbcc1320034cb257bf76a46fbaad1bd481c3f132
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-03-01 19:46:05 -0800

    Time: 326 ms (9.29%), Space: 25.8 MB (14.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,17 +6,18 @@ class Solution:
         output = 0
 
         def dfs(r, c):
-            visited.add((r, c))
             directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             for new_row, new_col in directions:
                 new_row += r
                 new_col += c
                 if (new_row, new_col) not in visited and new_row in range(rows) and new_col in range(cols) and grid[new_row][new_col] == "1":
+                    visited.add((new_row, new_col))
                     dfs(new_row, new_col)
 
         for r in range(rows):
             for c in range(cols):
                 if (r, c) not in visited and grid[r][c] == "1":
+                    visited.add((r, c))
                     dfs(r,c)
                     output += 1
         return output
