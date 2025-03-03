5ef5093dc88bd340c0c7f2324f5da6692126757c
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-03-03 01:21:20 -0800

    Time: 23 ms (19.87%), Space: 22.7 MB (26%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+        for string in strs:
+            count = [0] * 26
+            for c in string:
+                count[ord(c) - ord('a')] += 1
+            if tuple(count) not in dic:
+                dic[tuple(count)] = []
+            dic[tuple(count)].append(string)
+        return list(dic.values())
