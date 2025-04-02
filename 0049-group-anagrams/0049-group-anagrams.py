342dac7119d8d7065b96c501d986f6c92a1e4756
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-04-02 13:31:12 -0700

    Time: 2534 ms (5.16%), Space: 21.2 MB (44.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = {}
+        sorted_strs = []
         for string in strs:
-            count = [0] * 26
+            dummy = []
             for s in string:
-                count[ord(s) - ord("a")] += 1
-            if tuple(count) not in dic:
-                dic[tuple(count)] = []
-            dic[tuple(count)].append(string)
-        return list(dic.values())
+                dummy.append(s)
+            dummy.sort()
+            sorted_strs.append(''.join(dummy))
+        no_dup = set(sorted_strs)
+        output = []
+        for string in no_dup:
+            anagrams = []
+            for i in range(len(sorted_strs)):
+                if string == sorted_strs[i]:
+                    anagrams.append(strs[i])
+            output.append(anagrams)
+        return output
