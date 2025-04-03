5a148896cc9e183ab138a23181ea980eb8acfa9e
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-04-03 13:15:10 -0700

    Time: 35 ms (26.41%), Space: 27.2 MB (5.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,11 +3,8 @@ class Solution:
         left = [1]
         for i in range(1, len(nums)):
             left.append(left[i - 1] * nums[i - 1])
-        left.reverse()
-        nums.reverse()
-        right = 1
-        for i in range(len(nums)):
-            left[i] *= right
-            right *= nums[i]
-        left.reverse()
-        return left
+        right = [1]
+        for i in range(len(nums) - 2, -1, -1):
+            right.append(right[-1] * nums[i + 1])
+        right.reverse()
+        return [left[i] * right[i] for i in range(len(nums))]
