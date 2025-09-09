c81967a9c589e3a735ed33db7e3e8758b897b151
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-09-08 22:29:29 -0700

    Time: 27 ms (24.69%), Space: 12.5 MB (82.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,17 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        dic1 = {}
-        dic2 = {}
-        for i in range(len(s)):
-            if s[i] not in dic1:
-                dic1[s[i]] = 0
-            dic1[s[i]] += 1
-            if t[i] not in dic2:
-                dic2[t[i]] = 0
-            dic2[t[i]] += 1
-        if dic1 != dic2:
-            return False
-        return True
+        
+        from collections import Counter
+
+        dic1 = Counter(s)
+        dic2 = Counter(t)
+
+        if dic1 == dic2:
+            return True
+        
+        return False
