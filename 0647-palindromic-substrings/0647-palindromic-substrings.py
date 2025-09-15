abdfeebd773617e19e9219e5c19af7c83c4c1fdb
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-09-14 22:10:19 -0700

    Time: 127 ms (74.12%), Space: 17.7 MB (66.44%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -7,6 +7,7 @@ class Solution:
                 output += 1
                 l -= 1
                 r += 1
+        for i in range(len(s)):
             l, r = i , i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 output += 1
