0b24b44aed9ed59021859b50d5481e3d0ec09953
Abd Alrahman Mohammad <115138888+mdabdalrahman42@users.noreply.github.com>
2025-09-15 19:29:49 -0700

    Time: 3 ms (68.45%), Space: 17.8 MB (87.11%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -4,8 +4,8 @@ class Solution:
         dp[len(s)] = True
         for i in range(len(s) - 1, -1, -1):
             for word in wordDict:
-                if i + len(word) - 1 < len(s) and s[i:i + len(word)] == word:
-                    dp[i] = dp[i + len(word)]
                 if dp[i]:
                     break
+                if i + len(word) - 1 < len(s) and s[i:i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
         return dp[0]
